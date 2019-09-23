bfc8aacdcdc8b05d20738ea9283dd78a4ffa11f4
hanxiofei <627156393@qq.com>
2019-09-23 15:11:30 +0800

    up

---
+++ src/main/java/hxy/wcq/demo01/HelloController/IndexController.java
@@ -0,0 +1,13 @@
+package hxy.wcq.demo01.HelloController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String indexController(){
+        return "index";
+    }
+}
